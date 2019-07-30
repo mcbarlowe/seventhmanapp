5341a85c722e333eb11b990bd5421986686d0a35
mcbarlowe <mcbarlowe@gmail.com>
2019-07-30 01:13:05 -0400

    input boxes for stats table are now aligned and ready to pull data

--- src/components/input_form.js
+++ src/components/input_form.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
+import './css/input_form.css'
 
 class InputForm extends Component {
 
   render() {
     return (
       <form>
-        <label>
-          Season:
-        </label>
-        <input type="text" name="inputBox" value="test" />
+        <div class="formColumn">
+          <div class="formRow">
+            <div class="formItem">
+              <label class="formLabels">
+                Season:
+              </label>
+            </div>
+            <div class="formItem">
+              <input class="inputBox" type="text" name="inputBox" value="Season" />
+            </div>
+            <div class="formItem">
+              <label class="formLabels">
+                Player:
+              </label>
+            </div>
+            <div class="formItem">
+            <input class="inputBox" type="text" name="inputBox" value="Player" />
+            </div>
+          </div>
+          <div class="formRow">
+            <div class="formItem">
+              <label class="formLabels">
+                Team:
+              </label>
+            </div>
+            <div class="formItem">
+              <input class="inputBox" type="text" name="inputBox" value="Team" />
+            </div>
+            <div class="formItem">
+              <label class="formLabels">
+                Min TOC:
+              </label>
+            </div>
+            <div class="formItem">
+              <input class="inputBox" type="text" name="inputBox" value="Min TOC" />
+            </div>
+          </div>
+        </div>
       </form>
     )
   }
