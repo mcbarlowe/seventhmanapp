6bc7c3f22b16277b164c2539f5c5605275726526
mcbarlowe <mcbarlowe@gmail.com>
2019-09-19 21:21:08 -0400

    Added skeleton of site frame in react Tabs

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ class App extends Component {
 
   render() {
     const {teamSelect, playerSelect, seasonSelect} = this.state
+    const pStyle = {padding: "10px", width: "60%"}
     if(!playerSelect.length)
       return null;
     return (
@@ -49,26 +50,69 @@ class App extends Component {
       <div >
         <Tabs>
           <TabList>
+            <Tab>About</Tab>
             <Tab>Players</Tab>
             <Tab>Teams</Tab>
-            <Tab>About</Tab>
+            <Tab>Shot Charts</Tab>
+            <Tab>Articles</Tab>
           </TabList>
-
           <TabPanel>
-            <PlayerTable
-              seasonSelect={seasonSelect}
-              teamSelect={teamSelect}
-              playerSelect={playerSelect}
-            />
+            <p style={pStyle}>This is a website of NBA stats created by Matthew Barlowe. A lot of
+               the site is still in progress. If you
+               have any questions or comments or notice any errors please email me at
+               <a href='mailto:matt@theseventhman.net'> matt@seventhman.net</a> or
+               contact me on <a href='https://twitter.com/matt_barlowe'>Twitter</a>. Thanks
+               for coming and I hope you enjoy the site and find it useful.
+           </p>
           </TabPanel>
           <TabPanel>
-            <TeamTable
-              seasonSelect={seasonSelect}
-              teamSelect={teamSelect}
-            />
+            <Tabs>
+              <TabList>
+                <Tab>Per Game Stats</Tab>
+                <Tab>Possession Stats</Tab>
+                <Tab>Advanced Stats</Tab>
+              </TabList>
+              <TabPanel>
+                <PlayerTable
+                  seasonSelect={seasonSelect}
+                  teamSelect={teamSelect}
+                  playerSelect={playerSelect}
+                />
+              </TabPanel>
+              <TabPanel>
+                <p style={pStyle}>I'm working on it as fast as possible</p>
+              </TabPanel>
+              <TabPanel>
+                <p style={pStyle}>These are going to be super advanced because I am smrt</p>
+              </TabPanel>
+            </Tabs>
           </TabPanel>
           <TabPanel>
-            <p>Some about material here</p>
+            <Tabs>
+              <TabList>
+                <Tab>Per Game Stats</Tab>
+                <Tab>Possession Stats</Tab>
+                <Tab>Advanced Stats</Tab>
+              </TabList>
+              <TabPanel>
+                <TeamTable
+                  seasonSelect={seasonSelect}
+                  teamSelect={teamSelect}
+                />
+              </TabPanel>
+              <TabPanel>
+                <p style={pStyle}>I couldn't get this done because I didn't want it enough</p>
+              </TabPanel>
+              <TabPanel>
+                <p style={pStyle}>Why are we talking about advanced stats for teams when the only stat we need is wins baby!</p>
+              </TabPanel>
+            </Tabs>
+          </TabPanel>
+          <TabPanel>
+            <p style={pStyle}>Shot Charts and things of that nature will go here</p>
+          </TabPanel>
+          <TabPanel>
+            <p style={pStyle}>Plan to write things about basketball. It will be genius but you won't understand it so you'll tell all your friends it sucks</p>
           </TabPanel>
         </Tabs>
       </div>
